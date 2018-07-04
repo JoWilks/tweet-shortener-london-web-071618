85d4e69e274fa1bb1653ee8292e2e4d4dabf4594
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 16:01:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,15 @@
 def dictionary
   {
-    "hello" : "hi"
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
   }
 
 
